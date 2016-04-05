bb86f3dc0d9a5eae7641cbc0f6006775bf7f4466
Adam Chyła <adam@chyla.org>
2016-04-05 19:42:14 +0200

    Add timestamp field to agent logs

--- agent/src/main.cpp
+++ agent/src/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <boost/date_time/posix_time/posix_time_types.hpp>
 #include <boost/log/common.hpp>
 #include <boost/log/expressions.hpp>
 #include <boost/log/utility/setup/file.hpp>
 #include <boost/log/trivial.hpp>
+#include <boost/log/support/date_time.hpp>
+#include <boost/log/utility/setup/common_attributes.hpp>
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
@@ -16,9 +19,12 @@ int
 main(int argc, char *argv[])
 {
   try {
+    boost::log::add_common_attributes();
+
     boost::log::add_file_log(
       keywords::file_name = LOGDIR "/agent.log",
-      keywords::format = ( expr::stream << "<"
+      keywords::format = ( expr::stream << expr::format_date_time< boost::posix_time::ptime >("TimeStamp", "%d.%m.%Y %H:%M:%S")
+                                        << ": <"
                                         << boost::log::trivial::severity
                                         << ">\t"
                                         << expr::smessage
@@ -29,6 +35,8 @@ main(int argc, char *argv[])
 #ifdef HAVE_CONFIG_H
     BOOST_LOG_TRIVIAL(info) << "Version: " << VERSION;
 #endif
+
+    
   } catch (std::exception &ex) {
     std::cerr << ex.what() << '\n';
     BOOST_LOG_TRIVIAL(fatal) << ex.what();
