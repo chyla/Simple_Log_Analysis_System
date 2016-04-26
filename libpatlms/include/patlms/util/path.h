e80b557b1a57ecdd9085afe907067240fcc2bec6
Adam Chyła <adam@chyla.org>
2016-04-26 20:06:55 +0200

    Add function to check is absolute path correct

---
+++ libpatlms/include/patlms/util/path.h
@@ -0,0 +1,13 @@
+#ifndef LIBPATLMS_UTIL_PATH_H
+#define LIBPATLMS_UTIL_PATH_H
+
+#include <string>
+
+namespace util
+{
+
+bool IsAbsoluteUnixPath(const std::string &path);
+
+}
+
+#endif /* LIBPATLMS_UTIL_PATH_H */
---
+++ libpatlms/tests/util/path.cpp
@@ -0,0 +1,80 @@
+#include <gtest/gtest.h>
+#include <string>
+
+#include <patlms/util/path.h>
+
+using namespace testing;
+using namespace std;
+using namespace util;
+
+TEST(UtilTest, IsAbsoluteUnixPath_WithoutEndingShlash) {
+  string path = "/var/log/file";
+  
+  EXPECT_TRUE(IsAbsoluteUnixPath(path));
+}
+
+TEST(UtilTest, IsAbsoluteUnixPath_WithUnderscore) {
+  string path = "/va_r/l_og/fi_le";
+  
+  EXPECT_TRUE(IsAbsoluteUnixPath(path));
+}
+
+TEST(UtilTest, IsAbsoluteUnixPath_WithMinus) {
+  string path = "/v-ar/lo-g/fi-le";
+  
+  EXPECT_TRUE(IsAbsoluteUnixPath(path));
+}
+
+TEST(UtilTest, IsAbsoluteUnixPath_WithEndingShlash) {
+  string path = "/var/log/";
+  
+  EXPECT_TRUE(IsAbsoluteUnixPath(path));
+}
+
+TEST(UtilTest, IsAbsoluteUnixPath_WithTwoShlashes) {
+  string path = "/var/log//file";
+  
+  EXPECT_TRUE(IsAbsoluteUnixPath(path));
+}
+
+TEST(UtilTest, IsAbsoluteUnixPath_WithThreeShlashes) {
+  string path = "/var///log//file";
+  
+  EXPECT_TRUE(IsAbsoluteUnixPath(path));
+}
+
+TEST(UtilTest, IsAbsoluteUnixPath_WithThreeBeginingShlashes) {
+  string path = "///var/log//file";
+  
+  EXPECT_TRUE(IsAbsoluteUnixPath(path));
+}
+
+TEST(UtilTest, IsAbsoluteUnixPath_RelativePath) {
+  string path = "log/file";
+  
+  EXPECT_FALSE(IsAbsoluteUnixPath(path));
+}
+
+TEST(UtilTest, IsAbsoluteUnixPath_RelativePathOneDot) {
+  string path = ".";
+  
+  EXPECT_FALSE(IsAbsoluteUnixPath(path));
+}
+
+TEST(UtilTest, IsAbsoluteUnixPath_RelativePathTwoDots) {
+  string path = "..";
+  
+  EXPECT_FALSE(IsAbsoluteUnixPath(path));
+}
+
+TEST(UtilTest, IsAbsoluteUnixPath_RelativePathWithDots) {
+  string path = "../log/file";
+  
+  EXPECT_FALSE(IsAbsoluteUnixPath(path));
+}
+
+TEST(UtilTest, IsAbsoluteUnixPath_EmptyPath) {
+  string path = "";
+  
+  EXPECT_FALSE(IsAbsoluteUnixPath(path));
+}
