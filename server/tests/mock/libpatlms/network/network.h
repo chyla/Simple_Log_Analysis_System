433de6427e0da26e7cb0e77340929baf1471c5d2
Adam Chyła <adam@chyla.org>
2016-05-14 18:13:46 +0200

    Fix network mock in server

--- server/tests/mock/libpatlms/network/network.h
+++ server/tests/mock/libpatlms/network/network.h
@@ -15,7 +15,7 @@ namespace network
 
 class Network : public ::network::detail::NetworkInterface {
  public:
-  MOCK_METHOD1(Socket, int(int domain = PF_INET));
+  MOCK_METHOD1(Socket, int(int domain));
   
   MOCK_METHOD1(OpenUnixSocket, int(const std::string &path));
   MOCK_METHOD2(OpenIpv4Socket, int(const std::string &address, int port));
@@ -25,12 +25,12 @@ class Network : public ::network::detail::NetworkInterface {
   MOCK_METHOD2(SendText, void(int socket, const std::string &text));
   MOCK_METHOD1(ReceiveText, const std::string(int socket));
 
-  MOCK_METHOD1(RecvMessage, NetworkMessage(int socket));
-  MOCK_METHOD2(SendMessage, void(int socket, const NetworkMessage &message));
+  MOCK_METHOD1(RecvMessage, ::network::NetworkMessage(int socket));
+  MOCK_METHOD2(SendMessage, void(int socket, const ::network::NetworkMessage &message));
 
-  MOCK_METHOD2(WaitForData, WaitStatus(int socket, int timeout));
+  MOCK_METHOD2(WaitForData, ::network::WaitStatus(int socket, int timeout));
 
-  MOCK_METHOD1(Accept, ConnectionData(int socket));
+  MOCK_METHOD1(Accept, ::network::ConnectionData(int socket));
 };
 
 }
