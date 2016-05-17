ed654ab099b0846f6214daf69736ab62780def4e
Adam Chyła <adam@chyla.org>
2016-05-17 04:11:37 +0200

    Add Step 1 to Apache Anomaly Detection Configuration

--- web/patlms-web/apache/urls.py
+++ web/patlms-web/apache/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.status, name='status'),
+    url(r'^configure_anomaly_detection$', views.configure_anomaly_detection, name='configure_anomaly_detection')
 ]
--- web/patlms-web/apache/views.py
+++ web/patlms-web/apache/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render_to_response
 
 def status(request):
     return render_to_response("apache/status.html")
+
+def configure_anomaly_detection(request):
+    return render_to_response('apache/configure_anomaly_detection.html')
