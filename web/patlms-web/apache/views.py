0f288880056975c20dec8f875182808584872683
Adam Chyła <adam@chyla.org>
2016-05-17 05:23:45 +0200

    Add Actions Configuration page to Apache Logs app in web module

--- web/patlms-web/apache/views.py
+++ web/patlms-web/apache/views.py
@@ -2,6 +2,8 @@
 
 from django.shortcuts import render_to_response
 
+def configure_actions(request):
+    return render_to_response('apache/configure_actions.html')
 
 def status(request):
     return render_to_response("apache/status.html")
