38a24f64264490e807b4b33ecf5fa2a75e57fc69
Adam Chyła <adam@chyla.org>
2016-05-19 15:05:14 +0200

    Add missing log messages to CommandExecutor

--- server/src/web/command_executor.cpp
+++ server/src/web/command_executor.cpp
@@ -1,5 +1,6 @@
 #include "command_executor.h"
 
+#include <boost/log/trivial.hpp>
 #include <json/json.hpp>
 
 using namespace std;
@@ -9,25 +10,33 @@ namespace web
 {
 
 CommandExecutorPtr CommandExecutor::Create() {
+  BOOST_LOG_TRIVIAL(debug) << "web::CommandExecutor::Create: Function call";
   auto p = CommandExecutorPtr(new CommandExecutor());
   return p;
 }
 
 std::string CommandExecutor::Execute(std::string json_command) {
+  BOOST_LOG_TRIVIAL(debug) << "web::CommandExecutor::Execute: Function call";
+
   auto json_object = json::parse(json_command);
   string command = json_object["command"];
   string result = "{ \"status\" : \"unknown_command\" }";
 
+  BOOST_LOG_TRIVIAL(debug) << "web::CommandExecutor::Execute: json_command='" << json_command << "', command='" << command << "'";
+
   for (auto ptr : command_objects_) {
     if (ptr->IsCommandSupported(command)) {
+      BOOST_LOG_TRIVIAL(debug) << "web::CommandExecutor::Execute: Found object";
       result = ptr->Execute(json_command);
     }
   }
 
+  BOOST_LOG_TRIVIAL(debug) << "web::CommandExecutor::Execute: Returning: " << result;
   return result;
 }
 
 void CommandExecutor::RegisterCommandObject(detail::CommandExecutorObjectPtr object) {
+  BOOST_LOG_TRIVIAL(debug) << "web::CommandExecutor::RegisterCommandObject: Function call";
   command_objects_.insert(object);
 }
 
