19b3833af76574a94eb5a9b043d582aaf06798c5
Adam Chyła <adam@chyla.org>
2016-05-19 19:02:19 +0200

    Add communication with server to status bage in web module

--- web/patlms-web/general/views.py
+++ web/patlms-web/general/views.py
@@ -1,7 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from django.shortcuts import render_to_response
-
+import util
 
 def status(request):
-    return render_to_response("general/status.html")
+    exception = None
+    process_id = None
+    process_pidfile = None
+
+    try:
+        process_id = util.get_process_id_from_server()
+        process_pidfile = util.get_process_pidfile_path_from_server()
+    except IOError as e:
+        exception = e.strerror
+
+    return render_to_response("general/status.html", {'exception' : exception,
+                                                      'process_id' : process_id,
+                                                      'process_pidfile' : process_pidfile})
---
+++ web/patlms-web/util/util.py
@@ -0,0 +1,21 @@
+import json
+import copy
+
+from network import Network
+from config import SERVER_ADDRESS, SERVER_PORT
+
+def connect_and_get_result_from_command(command):
+    n = Network.connect_ipv4(SERVER_ADDRESS, SERVER_PORT)
+    Network.send_text(n, ' { "command" : "' + command + '" } ')
+
+    response = Network.receive_text(n)
+
+    json_response = json.loads(response)
+
+    return json_response['result']
+
+def get_process_id_from_server():
+    return connect_and_get_result_from_command('get_server_process_pid')
+
+def get_process_pidfile_path_from_server():
+    return connect_and_get_result_from_command('get_server_process_pidfile')
