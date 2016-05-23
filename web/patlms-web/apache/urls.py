bedcbcc223a9ec265003dcf005ee8d00597c9315
Adam Chyła <adam@chyla.org>
2016-05-23 11:22:38 +0200

    Rename Select date range page file

--- web/patlms-web/apache/urls.py
+++ web/patlms-web/apache/urls.py
@@ -6,6 +6,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.status, name='status'),
     url(r'^configure_actions', views.configure_actions, name='configure_actions'),
-    url(r'^configure_anomaly_detection$', views.configure_anomaly_detection, name='configure_anomaly_detection'),
+    url(r'^configure_anomaly_detection_select_data_range$', views.configure_anomaly_detection_select_data_range, name='configure_anomaly_detection_select_data_range'),
     url(r'^configure_anomaly_detection_step2$', views.configure_anomaly_detection_step2, name='configure_anomaly_detection_step2'),
 ]
--- web/patlms-web/apache/views.py
+++ web/patlms-web/apache/views.py
@@ -8,8 +8,8 @@ def configure_actions(request):
 def status(request):
     return render_to_response("apache/status.html")
 
-def configure_anomaly_detection(request):
-    return render_to_response('apache/configure_anomaly_detection.html')
+def configure_anomaly_detection_select_data_range(request):
+    return render_to_response('apache/configure_anomaly_detection_select_data_range.html')
 
 def configure_anomaly_detection_step2(request):
     return render_to_response('apache/configure_anomaly_detection_step2.html')
