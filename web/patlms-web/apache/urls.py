ded941c310caf5ba39f8b002462b54f59e06141c
Adam Chyła <adam@chyla.org>
2016-06-08 20:36:29 +0200

    Add Review detection results - select agent and virtualhost page

--- web/patlms-web/apache/urls.py
+++ web/patlms-web/apache/urls.py
@@ -14,4 +14,7 @@ urlpatterns = [
     url(r'^configure_anomaly_detection_select_data_range$', views.configure_anomaly_detection_select_data_range, name='configure_anomaly_detection_select_data_range'),
     url(r'^configure_anomaly_detection_correct_sessions_marks$', views.configure_anomaly_detection_correct_sessions_marks, name='configure_anomaly_detection_correct_sessions_marks'),
     url(r'^configure_anomaly_detection_save_settings$', views.configure_anomaly_detection_save_settings, name='configure_anomaly_detection_save_settings'),
+
+    url(r'^review_detection_results$', RedirectView.as_view(url='review_detection_results/select_agent_and_virtualhost', permanent=True), name='review_detection_results'),
+    url(r'^review_detection_results/select_agent_and_virtualhost', views.review_detection_results_select_agent_and_virtualhost, name='review_detection_results_select_agent_and_virtualhost'),
 ]
--- web/patlms-web/apache/views.py
+++ web/patlms-web/apache/views.py
@@ -109,3 +109,25 @@ def configure_anomaly_detection_save_settings(request):
                         args=(agent_name, virtualhost_name, begin_date, end_date, exception))
     else:
         return redirect('apache:status', permanent=False)
+
+def review_detection_results_select_agent_and_virtualhost(request):
+    agent_name = request.GET.get('agent_name', '')
+    virtualhost_name = request.GET.get('virtualhost_name', '')
+    exception = None
+    agent_names = None
+    virtualhosts_names = None
+
+    try:
+        agent_names = util.get_apache_agent_names()
+        virtualhosts_names = util.get_apache_virtualhost_names(agent_name)
+    except IOError as e:
+        exception = e.strerror
+
+    return render(request,
+                  'apache/review_detection_results/select_agent_and_virtualhost.html',
+                  {'exception' : exception,
+                   'agent_name' : agent_name,
+                   'agent_names' : agent_names,
+                   'virtualhost_name' : virtualhost_name,
+                   'virtualhosts_names' : virtualhosts_names,
+                   })
