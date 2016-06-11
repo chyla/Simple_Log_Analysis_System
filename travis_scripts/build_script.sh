055f9527fb00335af3d0ee7742236562dab29995
Adam Chyła <adam@chyla.org>
2016-06-11 13:28:23 +0200

    Remove valgrind check

--- travis_scripts/build_script.sh
+++ travis_scripts/build_script.sh
@@ -32,12 +32,6 @@ make -j 3
 make install
 make -j 3 check
 
-sed -i 's~<!-- <apparmor mode="disabled"/> -->~<apparmor mode="disabled"/>~' ${SERVER_INSTALL_DIR}/etc/patlms/dbus.config.template
-
-mkdir -p ${SERVER_INSTALL_DIR}/var/lib/patlms
-mkdir -p ${SERVER_INSTALL_DIR}/var/log/patlms
-mkdir -p ${SERVER_INSTALL_DIR}/var/run/patlms
-
 # build agent
 cd ${START_DIR}/agent
 ./autogen.sh
@@ -45,10 +39,6 @@ cd ${START_DIR}/agent
 make -j 3
 make install
 make -j 3 check
-
-mkdir -p ${AGENT_INSTALL_DIR}/var/lib/patlms
-mkdir -p ${AGENT_INSTALL_DIR}/var/log/patlms
-mkdir -p ${AGENT_INSTALL_DIR}/var/run/patlms
 
 # check web server
 cd ${START_DIR}/web
--- travis_scripts/script.sh
+++ travis_scripts/script.sh
@@ -4,5 +4,4 @@ set -e
 set -v
 
 travis_scripts/build_script.sh
-travis_scripts/check_with_valgrind.sh
 
