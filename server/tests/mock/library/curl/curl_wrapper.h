950992d41908461a1545dc1129a554389586810b
Adam Chyła <adam@chyla.org>
2016-06-28 01:28:40 +0200

    Add CurlWrapper mock

---
+++ server/tests/mock/library/curl/curl_wrapper.h
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2016 Adam Chyła, adam@chyla.org
+ * All rights reserved. Distributed under the terms of the MIT License.
+ */
+
+#ifndef MOCK_LIBRARY_CURL_CURL_WRAPPER_H
+#define MOCK_LIBRARY_CURL_CURL_WRAPPER_H
+
+#include "src/library/curl/detail/curl_wrapper_interface.h"
+
+#include <gmock/gmock.h>
+#include <memory>
+
+namespace mock
+{
+
+namespace library
+{
+
+namespace curl
+{
+
+class CurlWrapper;
+typedef std::shared_ptr<CurlWrapper> CurlWrapperPtr;
+
+class CurlWrapper : public ::library::curl::detail::CurlWrapperInterface {
+ public:
+  virtual ~CurlWrapper() = default;
+
+  static CurlWrapperPtr Create() {
+    return std::make_shared<CurlWrapper>();
+  }
+
+  MOCK_METHOD0(Init, CURL*());
+
+  MOCK_METHOD3(SetOpt, void(CURL* curl_handler, CURLoption option, void *parameter));
+
+  MOCK_METHOD1(Perform, void(CURL* curl_handler));
+
+  MOCK_METHOD1(Cleanup, void(CURL* curl_handler));
+
+  MOCK_METHOD2(SListAppend, curl_slist*(curl_slist *list, const std::string &text));
+
+  MOCK_METHOD1(SListFreeAll, void(curl_slist *list));
+};
+
+}
+
+}
+
+}
+
+#endif /* CURL_WRAPPER_H */
